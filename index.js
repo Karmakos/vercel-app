8ec021c1db24759eb2f1d255b4778b3cf740defc
karmakos <karimakos.murigi@gmail.com>
2024-02-09 02:43:03 +0300

    Modify the home view

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ import express from 'express'
 const port = '3000';
 const app = express();
 
-app.use('/', function (req, res){
-    res.send("Perfect")
-});
+
+app.use(express.static("public"));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res)=>
+	{
+		res.render("index.ejs");
+	});
 
 app.listen(port,
     console.log("App running")
